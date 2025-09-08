db028de133795364aa603fa756bf6c0098401f94
Valeriia Dubina <dvdlera@gmail.com>
2025-09-08 16:59:58 +0300

    ai logic fix

--- src/main/java/telegram/BotApp.java
+++ src/main/java/telegram/BotApp.java
@@ -6,11 +6,8 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
 import java.io.InputStream;
 import java.util.Properties;
-
-import static cn.hutool.core.lang.Console.print;
 
 
 public class BotApp extends MultiSessionTelegramBot {
